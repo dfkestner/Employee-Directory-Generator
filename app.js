81ba4971b2ddfeae6f687eee5009ce0f66661fdb
Tea Flores-Kestner <dfkestner@gmail.com>
2020-10-15 22:46:47 -0400

    fixed syntax issues, added functions to app.js to sort answers by employeeType and render HTML

--- app.js
+++ app.js
@@ -10,70 +10,102 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
+const employees = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-const questions = [
-    {
-        type: "list",
-        name: "employeeType",
-        message: "Select the job title of the team member you wish to add to the directory",
-        choices: ["Manager", "Engineer", "Intern", "I do not wish to add any more team members"]
-    },
-    {
-        type: "input",
-        name: "name",
-        message: "Please enter the team member's full name",
-        when: function(answers) {
-            return answers.employeeType !== "I do not wish to add any more team members";
-        }
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "Please enter the team member's ID",
-        when: function(answers) {
-            return answers.employeeType !== "I do not wish to add any more team members";
-        }
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Please enter the team member's e-mail address",
-        when: function(answers) {
-            return answers.employeeType !== "I do not wish to add any more team members";
-        }
-    },
-    {
-        type: "input",
-        name: "officeNumber",
-        message: "What is the manager's office number?",
-        when: function(answers) {
-            return answers.employeeType === "Manager";
-        }
-    },
-    {
-        type: "input",
-        name: "gitHub",
-        message: "What is the engineer's GitHub username?",
-        when: function(answers) {
-            return answers.employeeType === "Engineer";
-        }
-    },
-    {
-        type: "input",
-        name: "school",
-        message: "Where does the intern go to school?",
-        when: function(answers) {
-            return answers.employeeType === "Intern";
-        }
-    }
-]
+function addEmployees() {
+    console.log("Add a team member to the directory");
 
-inquirer.prompt(questions).then((answers) => {
-    console.log(JSON.stringify(answers, null, ""));
-    console.log(JSON.stringify(answers.employeeType, null, ""));
-})
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "employeeType",
+            message: "Select the job title of the team member you wish to add to the directory",
+            choices: ["Manager", "Engineer", "Intern", "I do not wish to add any more team members"]
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter the team member's full name",
+            when: function(answers) {
+                return answers.employeeType !== "I do not wish to add any more team members";
+            }
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Please enter the team member's ID",
+            when: function(answers) {
+                return answers.employeeType !== "I do not wish to add any more team members";
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter the team member's e-mail address",
+            when: function(answers) {
+                return answers.employeeType !== "I do not wish to add any more team members";
+            }
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the manager's office number?",
+            when: function(answers) {
+                return answers.employeeType === "Manager";
+            }
+        },
+        {
+            type: "input",
+            name: "gitHub",
+            message: "What is the engineer's GitHub username?",
+            when: function(answers) {
+                return answers.employeeType === "Engineer";
+            }
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Where does the intern go to school?",
+            when: function(answers) {
+                return answers.employeeType === "Intern";
+            }
+        }
+    ]).then(answers => {
+        console.log(JSON.stringify(answers, null, ""));
+        console.log(JSON.stringify(answers.employeeType, null, ""));
+    
+        if (answers.employeeType === "Manager") {
+            const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+            console.log(manager);
+            employees.push(manager);
+            addEmployees();
+        }
+        else if (answers.employeeType === "Engineer") {
+            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.gitHub);
+            console.log(engineer);
+            employees.push(engineer);
+            addEmployees();
+        }
+        else if (answers.employeeType === "Intern") {
+            const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+            console.log(intern);
+            employees.push(intern);
+            addEmployees();
+        }
+        else if (answers.employeeType === "I do not wish to add any more team members") {
+            console.log("Creating your directory...")
+            generateHTML();
+        }
+    })
+
+    function generateHTML() {
+        console.log(employees);
+        fs.writeFileSync(outputPath, render(employees), "utf8");
+    }
+}
+
+addEmployees();
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
